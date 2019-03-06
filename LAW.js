13895a50eb4c653bc790f189d53bc0419fffb845
jastro2 <jorbrown42@gmail.com>
2019-03-06 09:07:35 -0500

    Made the button on the landing page functional. Still need to figure out what to do for mobile, though.

---
+++ LAW.js
@@ -0,0 +1,11 @@
+/* Scroll */
+
+$('a[href^="#scrollHere"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+});
